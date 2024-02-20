433b12dcc7f16a709b77234ba525c0940ec1df6f
MaxGuns <153924950+MaxGuns@users.noreply.github.com>
2024-02-20 23:23:54 +0700

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -9,13 +9,13 @@ pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end
-      def on_stream_end(chat_id)
-         queues.task_done(chat_id)
+        def on_stream_end(chat_id)
+            queues.task_done(chat_id)
     
-      if queues.is_empty(chat_id):
-        pytgcalls.leave_group_call(chat_id)
-      else:
-        
+         if queues.is_empty(chat_id):
+            pytgcalls.leave_group_call(chat_id)
+            else:
+           
         pytgcalls.change_stream(
             chat_id, queues.get(chat_id)["file"]
         )
